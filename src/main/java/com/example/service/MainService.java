18ac6e09e3cd0981fb9cf7a9a0c85d328aee100e
JanaKorayem <janakorayem12@gmail.com>
2025-03-08 00:29:32 +0200

    Product modifications

--- src/main/java/com/example/service/MainService.java
+++ src/main/java/com/example/service/MainService.java
@@ -18,38 +18,21 @@ public abstract class MainService<T> {
         this.mainRepository = repository;
     }
 
-    /**
-     * Retrieves all entities from the system.
-     *
-     * @return A list of all entities.
-     */
     public ArrayList<T> findAll() {
         return mainRepository.findAll();
     }
 
-    /**
-     * Saves a single entity to the system.
-     *
-     * @param data The entity to save.
-     */
+
     public void save(T data) {
         mainRepository.save(data);
     }
 
-    /**
-     * Saves a list of entities to the system.
-     *
-     * @param data The list of entities to save.
-     */
+
     public void saveAll(ArrayList<T> data) {
         mainRepository.saveAll(data);
     }
 
-    /**
-     * Overwrites the entire data with the provided list of entities.
-     *
-     * @param data The list of entities to overwrite the existing data.
-     */
+
     public void overrideData(ArrayList<T> data) {
         mainRepository.overrideData(data);
     }
