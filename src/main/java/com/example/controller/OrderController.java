bf2d9db670dcfb666693c45afcec61b137233221
mirna <mirnahaitham4@gmail.com>
2025-03-10 22:55:16 +0200

    3ndy tany update

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -41,6 +41,6 @@ public class OrderController {
     @DeleteMapping("/delete/{orderId}")
     public String deleteOrderById(@PathVariable UUID orderId) {
         orderService.deleteOrderById(orderId);
-        return "Order deleted successfully!";
+        return "Order deleted successfully";
     }
 }
